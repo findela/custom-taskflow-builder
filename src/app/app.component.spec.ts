4d7f6e71abfceb466fad252c8b685b5d6859e7d4
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2022-01-28 23:54:08 +0530

    Updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'startsmart-customer'`, () => {
+  it(`should have as title 'angular-setup'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('startsmart-customer');
+    expect(app.title).toEqual('angular-setup');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('startsmart-customer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-setup app is running!');
   });
 });
