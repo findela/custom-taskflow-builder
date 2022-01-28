e1411c367d149a2b4e8cee42157b96c511e7dd09
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2022-01-28 23:56:58 +0530

    Updated

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+/*
+  This file consists of settings and configurations
+  which are exclusively used for ****hmr enabled*** environment.
+*/
+export const environment = {
+  production: true,
+  hmr: false,
+  environmentName: 'production',
+  apiUrl: '',
+  mockApiUrl: ' https://demo7389246.mockable.io'
+};
